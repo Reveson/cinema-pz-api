a673912b88cdbfc7d357267608ed3062ffcd7c61
mateusz <themaatej@gmail.com>
2020-01-08 22:23:03 +0100

    Added movie dto objects

---
+++ src/main/java/com/example/cinema/cinemapz/dto/MovieDto.java
@@ -0,0 +1,60 @@
+package com.example.cinema.cinemapz.dto;
+
+public class MovieDto {
+
+    private int id;
+    private String name;
+    private String categoryName;
+    private String description;
+    private String age;
+    private String duration;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
